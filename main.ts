a8b462b4dfdd22c3099e493752eb6a2e1e9b51c8
brunobc <brunobezerrachaves@gmail.com>
2017-04-22 15:42:47 -0300

    retryWhen

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 let output = document.getElementById('output');
 let button = document.getElementById('button');
@@ -9,14 +9,30 @@ function load(url: string) {
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener('load', () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         });
 
         xhr.open('GET', url);
         xhr.send();
-    });
+    }).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+}
+
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+    return function (errors) {
+        return errors
+        .scan((acc, value) => {
+            console.log(acc, value);
+            return acc + 1;
+        }, 0)
+        .takeWhile(acc => acc < attempts)
+        .delay(delay);
+    }
 }
 
 function renderMovies(movies) {
@@ -26,11 +42,11 @@ function renderMovies(movies) {
         output.appendChild(div);
     });
 }
-load('movies.json').subscribe(renderMovies);
+//load('movies.json').subscribe(renderMovies);
 
 click.flatMap(e => load('movies.json'))
     .subscribe(
     renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log('complete')
-);
+    );
