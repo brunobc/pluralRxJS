7652c1b3505a4b98ddc08671c4110dbd490e284a
brunobc <brunobezerrachaves@gmail.com>
2017-04-22 14:58:59 -0300

    flatMap

--- main.ts
+++ main.ts
@@ -1,23 +1,36 @@
 import {Observable } from 'rxjs';
 
-let circle = document.getElementById('circle');
-let source = Observable.fromEvent(document, 'mousemove')
-    .map((e: MouseEvent) => {
-        return {
-            x: e.clientX,
-            y: e.clientY
-        }
-    })
-    .filter(value => value.x < 500)
-    .delay(300);
+let output = document.getElementById('output');
+let button = document.getElementById('button');
+let click = Observable.fromEvent(document, 'click')
 
-function onNext(value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        });
+
+        xhr.open('GET', url);
+        xhr.send();
+    });
 }
 
-source.subscribe(
-    onNext,
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement('div');
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+load('movies.json').subscribe(renderMovies);
+
+click.flatMap(e => load('movies.json'))
+    .subscribe(
+    renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log('complete')
 );
